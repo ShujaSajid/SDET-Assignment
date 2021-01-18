f9581fc0be924146cf257060588d21df70ef384e
ShujaSajid <shujasajid@outlook.com>
2021-01-18 23:23:27 +0530

    Starting Solution.

---
+++ solution.py
@@ -0,0 +1,15 @@
+#Question:- ​If we list all the natural numbers below 10 that are multiples of 3 and 5, weget 3, 5, 6 and 9.
+#The sum of these multiples is 23 and reverse is 32.
+# Write a program inyour favourite programming language to find the sum of all the multiples of X and Ybelow N and also print the reverse of that sum.
+
+#Pyhton program for the above mentioned problem.
+
+def findSum(n, x, y):
+    sum = 0
+    pass
+
+
+def revSum(sum):
+    pass
+
+
