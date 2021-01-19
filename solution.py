ca287a6f2d3afd0d6274a3423f8508cacec47b18
ShujaSajid <shujasajid@outlook.com>
2021-01-19 13:07:42 +0530

    Completed the program in solution.py
    This is a terminal based program as this will take  valuse from the user.

--- solution.py
+++ solution.py
@@ -1,15 +1,40 @@
-#Question:- ​If we list all the natural numbers below 10 that are multiples of 3 and 5, weget 3, 5, 6 and 9.
-#The sum of these multiples is 23 and reverse is 32.
-# Write a program inyour favourite programming language to find the sum of all the multiples of X and Ybelow N and also print the reverse of that sum.
+"""
+Function to return the sum of all 
+the integers below N which are 
+multiples of either X or Y
+"""
 
-#Pyhton program for the above mentioned problem.
-
-def findSum(n, x, y):
+def find_sum(n, x, y):
     sum = 0
-    pass
+    for i in range(0, n, 1):
+         
+        # If i in a multiple of x or y
+        if (i % x == 0 or i % y == 0):
+            sum += i 
+    return sum
 
+"""
+Now to reverse the obtained sum of the multiples
+"""
 
-def revSum(sum):
-    pass
+def reverse_integer(Number):
+    reverse = 0
+    while(Number > 0):
+        reminder = Number %10
+        reverse = (reverse *10) + reminder
+        Number = Number //10
+    return reverse
+
+if __name__ == '__main__':
+    n = int(input("Enter the value of N :"))
+    x = int(input("Enter the value of X :"))
+    y = int(input("Enter the value of Y :"))
+   
+    sumMultiples= find_sum(n, x, y)
+    
+    print(f"The Sum of multiples of {x} and {y} is {sumMultiples}")
+    reverse = reverse_integer(sumMultiples)
+
+    print(f"The reverse of the obtainde {sumMultiples} is {reverse}")
 
 
